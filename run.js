2f9b36fb78d2cafd128e9f9478cb6135af9a8503
Fernando Godinho <CainVampyr@gmail.com>
2015-06-11 00:15:46 +0100

    Revert "Jade"

--- run.js
+++ run.js
@@ -7,6 +7,5 @@ var helloWorld      = new require(__base + "server/lessons/helloWorld")(3000);
 var staticFiles     = new require(__base + "server/lessons/staticFiles")(3001);
 var basicRouting    = new require(__base + "server/lessons/basicRouting")(3002);
 var simpleRouter    = new require(__base + "server/lessons/simpleRouter")(3003);
-var jadeTemplates   = new require(__base + "server/lessons/JadeTemplates")(3004);
 
 console.log("All up and running!");
