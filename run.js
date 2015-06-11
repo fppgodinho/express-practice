37506306c61a8c6e4f456b6c278930157640131b
Fernando <fernando.godinho@gamesys.co.uk>
2015-06-11 17:02:25 +0100

    Just preparing the branch for a merge with mixed-templates

--- run.js
+++ run.js
@@ -1,11 +1,19 @@
 /**
  * Created by Cain on 09-06-2015.
  */
-global.__base   = __dirname + '/';
+global.__base           = __dirname + '/';
 
-var helloWorld      = new require(__base + "server/lessons/helloWorld")(3000);
-var staticFiles     = new require(__base + "server/lessons/staticFiles")(3001);
-var basicRouting    = new require(__base + "server/lessons/basicRouting")(3002);
-var simpleRouter    = new require(__base + "server/lessons/simpleRouter")(3003);
+var moduleJade          = new require(__base + "server/modules/moduleJade");
+var moduleEJS           = new require(__base + "server/modules/moduleEJS");
+var controllerServer    = new require(__base + "server/controllerServer");
+
+var server              = new controllerServer(3000);
+moduleJade.decorate(server.app, server.router);
+moduleEJS.decorate(server.app, server.router);
+server.start();
+
+
+
+
 
 console.log("All up and running!");
